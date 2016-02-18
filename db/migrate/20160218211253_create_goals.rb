7568197e3b5115e2494d37a5e8b3dc3a2be4941c
Jorge <floresjmjr@gmail.com>
2016-02-18 18:32:45 -0500

    modified goal migration file and executed it

--- db/migrate/20160218211253_create_goals.rb
+++ db/migrate/20160218211253_create_goals.rb
@@ -1,6 +1,8 @@
 class CreateGoals < ActiveRecord::Migration
   def change
     create_table :goals do |t|
+    	t.string :description
+    	t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
