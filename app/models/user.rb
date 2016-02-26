93c221d9b36825fc4733fa05fe37325b81a90ff0
Jorge <floresjmjr@gmail.com>
2016-02-26 15:19:19 -0500

    email not case-sensative anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
 	has_many :goals
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_save do  
+  	self.email = email.downcase
+  end
+
 end
