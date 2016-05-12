3b7a3349e838313b79a33c80ac13c663abce2d4c
Jorge <floresjmjr@gmail.com>
2016-05-12 11:55:21 -0400

    friend request phase one, model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422133833) do
+ActiveRecord::Schema.define(version: 20160429043122) do
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friends", force: :cascade do |t|
     t.datetime "created_at", null: false
